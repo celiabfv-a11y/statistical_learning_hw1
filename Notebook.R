784772eab6a7884f9c2275ef0ac85ff0a341c57d
100550985-star <100550985@alumnos.uc3m.es>
2025-11-09 11:53:38 +0100

    Bind and print factor analysis results
    Add results binding and printing for factor analysis.

--- Notebook.R
+++ Notebook.R
@@ -284,6 +284,10 @@ loadings(fa3)
 fa2 = factanal(X, factors = 2, scores = "Bartlett", rotation = "varimax")
 fa2
 loadings(fa2)
+results = cbind(fa3$loadings, fa3$uniquenesses)
+colnames(results) = c("factor1", "factor2","factor3" ,"uniquenesses")
+print(results)
+                              
 ```
 
 # Clustering tools
